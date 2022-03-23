44e8732b14eacf0fa296965d14433d3f83cd4d25
kyra qiu <kyraqiu@gmail.com>
2022-03-22 22:11:19 -0400

    commit tree 097d267b7307dc9a03c1fc0c71e4f320cbc3ed37

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+// Code Along
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
 // Code your solutions in this file
+
+function writeCards(name,event){
+    let thankYou = [];
+
+    for (let i=0; i < name.length; i++)
+    {thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);}
+    return thankYou;
+}
+
+function countDown(){
+    let countDown = 10;
+    while (countDown > -1 ) {
+      console.log(countDown--);
+      debugger;
+    }
+  }
+  
+  countDown(10);
